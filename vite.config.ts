a97f3a5795de31b944440c087ceb789d53b60f6e
halo <2862440280@qq.com>
2026-09-14 19:35:08 +0800

    chore: remove unused indicator window entry points

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,6 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         toolbar: resolve(__dirname, "toolbar.html"),
-        indicator: resolve(__dirname, "indicator.html"),
       },
     },
   },
